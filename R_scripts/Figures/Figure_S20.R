91dc5121751bc867b33aeeb40a7deb6aa75b3e69
ja-Reeve <82411887+ja-Reeve@users.noreply.github.com>
2023-04-17 16:09:33 +0200

    Update Figure_S20.R
    New version

--- R_scripts/Figures/Figure_S20.R
+++ R_scripts/Figures/Figure_S20.R
@@ -3,7 +3,7 @@
 ### Preparation
 rm(list = ls())
 dev.off()
-setwd("")
+setwd("/Users/james/Documents/Inversion_detection/")
 options(stringsAsFactors = FALSE)
 
 ### Packages
@@ -11,37 +11,29 @@ library(tidyverse)
 library(ggpubr)
 
 ### List of inversions
-INVs <- c("LGC1.1", "LGC1.2", "LGC2.1", "LGC4.1", "LGC5.1", "LGC6.1-2", "LGC7.1", 
-          "LGC7.2", "LGC9.1", "LGC9.2", "LGC10.1", "LGC10.2", "LGC11.1", "LGC12.1", 
-          "LGC12.2", "LGC12.3", "LGC12.4", "LGC14.1", "LGC14.2", "LGC14.3", "LGC17.1")
+# Note: LGC5.1, LGC14.2 and LGC14.3 dropped due to uncertain clustering
+INVs <- c("LGC1.1", "LGC1.2", "LGC2.1", "LGC4.1", "LGC6.1-2", "LGC7.1", "LGC7.2", 
+          "LGC9.1", "LGC9.2", "LGC10.1", "LGC10.2", "LGC11.1", "LGC12.1", "LGC12.2", 
+          "LGC12.3", "LGC12.4", "LGC14.1", "LGC17.1")
 
 ### Set colour and shape scales
 # Colour by genotype
 K3_col <- c("#1f78b4", "#a4db77ff", "#e31a1c")
-K6_col <- c("#1f78b4", "#a6cee3", "#33a02c", "#b2df8a", "#fb9a99", "#e31a1c")
+K6_col <- c("#1f78b4", "#a6cee3", "#33a02c", "#fb9a99", "#e31a1c")
 # Shape by ecotype
-EC_col <- c(1, 17, 15, 18, 8, 19)
+EC_col <- c(1, 17, 15, 18, 19)
 
 
 ### PCA plot function
 PCA.plotter <- function(inversion){
   
-  if(inversion != "LGC6.1-2") LG <- gsub("C", "", gsub("*[.].", "", inversion)) else LG <- "LG6"
-  
+  if(inversion != "LGC6.1-2") {LG <- gsub("C", "", gsub("*[.].", "", inversion))} else {LG <- "LG6"}
+   
   ## Download PCA data
-  dat <- read.csv(paste0("PCA_per_inversion/", LG, "_PCA_of_", inversion, "_arc-sax.csv"), header = TRUE)
-  # Renaming columns to remove inversion prefix
-  colnames(dat) <- if(inversion == "LGC6.1-2"){
-    gsub("LGC6.1.2.", "", colnames(dat))
-  } else {
-    gsub(paste0(inversion,"."), "", colnames(dat))}
-  
-  ## Stop function if K != 3 | 6
-  if(length(unique(dat$genotype)) != 3 & length(unique(dat$genotype)) != 6) {
-    stop(paste0("Error: ", inversion, " does not form 3 or 6 genotype clusters!"))}
+  dat <- read.csv(paste0("PCA_per_inversion/", LG, "_PCA_of_", inversion, "_v2_arc-sax.csv"), header = TRUE)
   
   ## Download percentage varaince on PCA axes
-  Pvar <- read.csv("PCA_per_inversion/Percentage_varaince_explained.csv", header = TRUE)
+  Pvar <- read.csv("PCA_per_inversion/Percentage_varaince_explained_v2.csv", header = TRUE)
   Pvar <- Pvar[Pvar$LG == LG & Pvar$Inv == inversion,]
   
   ## PCA plot
@@ -67,10 +59,9 @@ PCA.plotter <- function(inversion){
   return(P)
 }
 
-# Make plot list
-P <- lapply(INVs, function(i)try(PCA.plotter(i)))
-# Filter out inversions which returned an error
-P <- P[sapply(P, is.ggplot)]
+figS20 <-ggarrange(plotlist = lapply(INVs, PCA.plotter), ncol = 4, nrow = 5)
 
-### Multi-panel plot
-ggarrange(plotlist = P, ncol = 3, nrow = 6)
+### Save plot
+tiff("plots/Supp_mat_plots/FigS20.tiff", width = 42, height = 52, units = "cm", res = 300)
+figS20
+dev.off()
